184d2d585969673216633d697d2871416eb01cc6
kilik42 <marvinevins@yahoo.com>
2021-08-08 13:31:13 -0500

    first commit

---
+++ Hotel.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.8.6;
+
+contract HotelRoom {
+       
+        address payable public owner;
+        //vacant or occupied
+        enum Statuses {
+            Vacant, 
+            Occupied
+        };
+        Statuses currentStatus;
+        
+        //events
+        
+
+
+        constructor() public{
+                owner = msg.sender;
+                currentStatus = Statuses.Vacant;
+        }
+
+        modifier onlyWhileVacant {
+            require(currentStatus = Statuses.Vacant, "Currently occupied");
+        }
+
+        modifier priceCheck(uint _amount ) {
+                     require(msg.value >=  _amount, "you need more ether. Not enough ether provided.");
+        }
+
+        function book() payable onlyWhileVacant, priceCheck(2 ether) {
+         
+                //pay owner to book hotel room
+                currentStatus = Statuses.Occupied;
+                owner.transfer(msg.value);
+
+        }
+
+}
+
+
